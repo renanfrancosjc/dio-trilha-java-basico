1130d0f74bce5cc37782bdbf1f898976d76ea982
renanfrancosjc <renanfranco.sjc@hotmail.com>
2024-05-19 22:50:12 -0300

    atualizando projeto de tipos de variaveis dio

---
+++ src/Tipos_variaveis.java
@@ -0,0 +1,11 @@
+public Class Tipos_Variaveis{
+    public static void main(String[]args){
+        int numero = 5
+
+        numero = numero + 2
+        numero++
+
+        numero == numero ? "verdadeiro" : "falso"
+    }
+}
+
