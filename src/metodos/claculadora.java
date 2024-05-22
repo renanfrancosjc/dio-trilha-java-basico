4dd50f5d90655f398f9c784dec9661480e57a5cb
renanfrancosjc <renanfranco.sjc@hotmail.com>
2024-05-21 23:05:22 -0300

    Documentação e boas praticas com java

---
+++ src/metodos/claculadora.java
@@ -0,0 +1,15 @@
+
+@author Renan Franco
+@version 1.0
+@sice 21/05/2024
+public class Calculadora{
+    public static void main(String[] args) {
+        @param numeroUm
+        @param numeroDois
+        @retorno
+
+        public int somar(int numeroUm, int numeroDois){
+            return numeroUm + numeroDois;
+        }
+    }
+}
